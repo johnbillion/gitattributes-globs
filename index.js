93eafa8a69ade24966c67ab4615e38fcc3c68bc2
John Blackbourn <johnbillion@gmail.com>
2019-02-03 01:08:40 +0100

    Convert the library to operate on export-ignore entries in a .gitattributes file.

--- index.js
+++ index.js
@@ -1,51 +1,31 @@
 'use strict';
 
 var fs = require('fs');
+var gg = require('gitignore-globs');
 
-// Prepare removes blank lines and comments.
+// Prepare removes all lines that don't concern export-ignore.
 function prepare(patterns) {
     return patterns.filter(function(pattern) {
         pattern = pattern.trim();
-        return pattern.charAt(0) !== '#' && pattern !== '';
+        if ( pattern.charAt(0) === '#' ) {
+            return false;
+        }
+        return / export-ignore$/.test( pattern );
     });
 }
 
-// Map transforms .gitignore patterns into globs.
+// Map transforms export-ignore entries into plain file and directory names.
 function map(patterns) {
-    // Account for files and directories.
     patterns = patterns.map(function(pattern) {
-        if (!isFile(pattern)) {
-            var suffix = (pattern.slice(-1) == '*') ? '*' : '/**';
-            if (pattern.slice(-1) === '/') {
-              pattern = [pattern + suffix];
-            } else {
-              // Create pair of globs.
-                pattern = [pattern, pattern + suffix];
-            }
-        }
-        return pattern;
-    });
-
-    // Flatten previously created glob pairs.
-    // TODO: Find a combined solution.
-    patterns = Array.prototype.concat.apply([], patterns);
-
-    // Account for subdirectories.
-    patterns = patterns.map(function(pattern) {
-        if (!isFile(pattern)) {
-            if (pattern.charAt(0) !== '/') {
-                return '**/' + pattern;
-            }
-        }
-        return pattern;
+        return pattern.trim().replace( / export-ignore$/, '' ).trim();
     });
 
     return patterns;
 }
 
-// Parse a .gitignore file and return its glob patterns as an array.
+// Parse a .gitattributes file and return its export-ignore entries as glob patterns in an array.
 function parse(file, options) {
-    var file = file || '.gitignore';
+    var file = file || '.gitattributes';
     var options = options || {};
     options.negate = options.negate || false;
 
@@ -53,9 +33,12 @@ function parse(file, options) {
     var patterns = content.split('\n');
 
     patterns = prepare(patterns);
-    var globs = map(patterns);
+    patterns = map(patterns);
+
+    patterns = gg._prepare(patterns);
+    var globs = gg._map(patterns);
     if (options.negate) {
-        globs = negate(globs);
+        globs = gg._negate(globs);
     }
 
     return globs;
--- test/index.js
+++ test/index.js
@@ -4,57 +4,15 @@ var expect = require('chai').expect;
 var parse = require('../index');
 
 describe('Prepare', function() {
-    it('should ignore blank lines', function() {
-        var result = parse._prepare(['foo', '', 'bar']);
+    it('should ignore unrelated lines', function() {
+        var result = parse._prepare(['foo', '', 'bar export-ignore']);
 
-        expect(result).to.eql(['foo', 'bar']);
+        expect(result).to.eql(['bar export-ignore']);
     });
 
     it('should ignore comments', function() {
-        var result = parse._prepare(['foo', '# Comment', 'bar']);
+        var result = parse._prepare(['# foo export-ignore', 'bar export-ignore']);
 
-        expect(result).to.eql(result, ['foo', 'bar']);
-    });
-});
-
-describe('Globs', function() {
-    it('should match root', function() {
-        var files = ['/foo', '/foo/', 'bar', 'bar/foo', 'bar/foo/baz'];
-        var patterns = ['/foo'];
-
-        var globs = parse._map(patterns);
-        var result = multimatch(files, globs);
-
-        expect(result).to.eql(['/foo', '/foo/']);
-    });
-
-    it('should match sub', function() {
-        var files = ['/foo', '/foo/', 'bar', 'bar/foo', 'bar/foo/baz'];
-        var patterns = ['foo'];
-
-        var globs = parse._map(patterns);
-        var result = multimatch(files, globs);
-
-        expect(result).to.eql(['/foo', '/foo/', 'bar/foo', 'bar/foo/baz']);
-    });
-
-    it('should match dir', function() {
-        var files = ['/foo', '/foo/', 'bar', 'bar/foo', 'bar/foo/baz'];
-        var patterns = ['foo/'];
-
-        var globs = parse._map(patterns);
-        var result = multimatch(files, globs);
-
-        expect(result).to.eql(['/foo/', 'bar/foo/baz']);
-    });
-
-    it('should match glob pattern', function() {
-        var files = ['bar', 'foo/', 'foo/bar', 'foo/bar/baz'];
-        var patterns = ['foo/*'];
-
-        var globs = parse._map(patterns);
-        var result = multimatch(files, globs);
-
-        expect(result).to.eql(['foo/bar', 'foo/', 'foo/bar/baz']);
+        expect(result).to.eql(['bar export-ignore']);
     });
 });
